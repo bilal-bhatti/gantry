3db6aab4a5ec78837f31d99613cd59436b5abb7a
Bilal Bhatti <1070897+bilal-bhatti@users.noreply.github.com>
2025-07-24 23:25:33 -0500

    fix tests

--- src/test/java/com/scuid/rx/PolicyTest.java
+++ src/test/java/com/scuid/rx/PolicyTest.java
@@ -5,19 +5,19 @@ import org.junit.Test;
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.node.ObjectNode;
 import com.google.inject.Module;
-import com.alchemain.rx.delegates.person.EvaluatePolicy;
+import com.alchemain.rx.delegates.NoOp;
 import com.alchemain.rx.init.GuiceFactory;
 import com.alchemain.rx.init.MongoModule;
-import com.alchemain.rx.init.KasbaModule;
+import com.alchemain.rx.init.ReactorModule;
 import com.alchemain.rx.internal.ReactorCore;
 import com.alchemain.rx.messages.ExecutionContext;
-import com.scuid.commons.json.JsonFiles;
-import com.scuid.commons.json.JsonProvider;
+import com.alchemain.rx.bus.JsonFiles;
+import com.alchemain.rx.bus.JsonProvider;
 
 public class PolicyTest {
 	@Test
 	public void testNewPerson() throws Exception {
-		Module[] m = new Module[] { new KasbaModule(), new MongoModule() };
+		Module[] m = new Module[] { new ReactorModule(), new MongoModule() };
 		GuiceFactory.initialize(m);
 
 		ReactorCore core = GuiceFactory.injector().getInstance(ReactorCore.class);
@@ -30,7 +30,7 @@ public class PolicyTest {
 		data.put("type", "CREATE");
 		data.put("entity", person);
 
-		String id = core.ask(ctx, EvaluatePolicy.class, data);
+		String id = core.ask(ctx, NoOp.class, data);
 		System.out.println("job id: " + id);
 
 		Thread.sleep(1000 * 5);
